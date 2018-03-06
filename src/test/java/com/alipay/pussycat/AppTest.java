372a35f2aa512c7e68efae93f7f09a58364b652c
wb-yejiadong <wb-yejiadong@alibaba-inc.com>
2018-03-06 16:04:42 +0800

    update

--- src/test/java/com/alipay/pussycat/AppTest.java
+++ src/test/java/com/alipay/pussycat/AppTest.java
@@ -1,38 +1,22 @@
 package com.alipay.pussycat;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alipay.pussycat.base.BaseJunit4Test;
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest extends BaseJunit4Test{
+
+    @Autowired
+    private DemoService demoService;
+
+    @Test
+    public void testGetService() {
+        System.out.println(demoService);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
