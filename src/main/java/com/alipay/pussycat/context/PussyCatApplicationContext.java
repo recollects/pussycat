263ea3eba33b404ae865a0e3fe87091c0d60a129
wb-yejiadong <wb-yejiadong@alibaba-inc.com>
2018-03-13 09:37:06 +0800

    update

--- src/main/java/com/alipay/pussycat/context/PussyCatApplicationContext.java
+++ src/main/java/com/alipay/pussycat/context/PussyCatApplicationContext.java
@@ -15,7 +15,7 @@ public class PussyCatApplicationContext implements ApplicationContextAware {
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext=applicationContext;
+        PussyCatApplicationContext.applicationContext=applicationContext;
     }
 
 
