1478059fae7ade04dd981c733ad6010536c02643
recollects <recollects@aliyun.com>
2018-06-11 15:13:50 +0800

    加入 注解

---
+++ core/api/src/main/java/com/alipay/pussycat/core/api/annotation/Reference.java
@@ -0,0 +1,55 @@
+package com.alipay.pussycat.core.api.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author recollects
+ * @version V1.0
+ * @date 2018年06月11日 下午3:00
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface Reference {
+
+    /**
+     *接口名
+     *
+     * @return
+     */
+    String interfaceName() default "";
+
+    /**
+     *
+     * 版本号
+     * @return
+     */
+    String version() default "";
+
+    /**
+     *url,如果有地址就采用url地址方式,如果没有就去注册中心获取
+     *
+     * @return
+     */
+    String[] url() default "";
+
+    /**
+     *代理方式
+     *
+     * @return
+     */
+    String proxy() default "";
+
+    /**
+     *超时时间
+     * @return
+     */
+    int timeout() default 0;
+
+    /**
+     * 是否校验,启动的时候是否先校验一下提供服务端是否通
+     *
+     * @return
+     */
+    boolean check() default false;
+}
---
+++ core/api/src/main/java/com/alipay/pussycat/core/api/annotation/Service.java
@@ -0,0 +1,50 @@
+package com.alipay.pussycat.core.api.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author recollects
+ * @version V1.0
+ * @date 2018年06月11日 下午2:59
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface Service {
+
+    /**
+     * 版本号
+     * @return
+     */
+    String version() default "";
+
+    /**
+     * 超时时间
+     * @return
+     */
+    int timeout() default 0;
+
+    /**
+     * 接口名
+     * @return
+     */
+    String interfaceName() default "";
+
+    /**
+     * 安全考虑,或者基于白名单的方式都可以,预留
+     * @return
+     */
+    String token() default "";
+
+    /**
+     * 项目启动是否注册到注册中心
+     * @return
+     */
+    boolean register() default false;
+
+    /**
+     * 异步调用,给客户端一个future
+     * @return
+     */
+    boolean async() default false;
+}
